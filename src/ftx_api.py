32fe9184c38fa64fd93024ab2cedcb8e60829322
Sameer Fulzele <fulzelesameer@gmail.com>
2021-12-27 20:01:10 +0530

    UPDATE: requirements.txt

--- src/ftx_api.py
+++ src/ftx_api.py
@@ -33,38 +33,3 @@ def get_historical_data(base_currency,
     df_hist = df_hist[['open_price','high_price','low_price','close_price','volume','time']]
 
     return df_hist
-
-        
-
-    
-
-def fetch_price_from_ftx(df_tweets):
-    #FTX Fetch prices
-    df_ftx = pd.DataFrame()
-    for i in range(len(df_tweets)):
-        df_temp = df_tweets.iloc[i,:]
-        tweet_time = df_temp['created_at']
-        base_currency = df_temp['coin']
-        quote_currency = 'USD'
-        
-        min_interval=str(60)
-        
-        
-        tweet_time = pd.to_datetime(tweet_time)
-        start_date = datetime.timestamp(tweet_time)
-        
-        end_datetime = tweet_time + pd.DateOffset(hours=12)
-        end_date = datetime.timestamp(end_datetime)    
-
-        
-
-        
-
-        df_hist_of_coin = pd.DataFrame(historical['result'])
-
-        
-        df_hist_of_coin['coin_pair'] = base_currency + quote_currency
-        print(base_currency + quote_currency,len(df_hist_of_coin))
-        df_ftx = df_ftx.append(df_hist_of_coin, ignore_index= True)
-    df_ftx.to_csv('data/price_data_ftx.csv',index = False)
-    print('Prices data fetched from FTX has been saved in data folder')
